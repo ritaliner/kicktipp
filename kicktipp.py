176f39c30015298e9662b14733a444ba51da5c76
ritaliner <40183871+ritaliner@users.noreply.github.com>
2018-06-12 12:19:58 +0200

    Add files via upload

--- kicktipp.py
+++ kicktipp.py
@@ -4,29 +4,27 @@ from bs4 import BeautifulSoup
 
 
 
-
+urls = 'https://www.kicktipp.at/wm-partypatrioten/gesamtuebersicht?ansicht=platzierungen&'
+list_names = 0
+# dic = {}
 
 
 def get_data():
-	html = requests.get(urls).content
-	soup = BeautifulSoup(html, "lxml")
-	f = open('soup.html', 'wb')
-	f.write(soup.prettify().encode('utf8'))
-	data = soup.find_all('div', 'mg_name')
-	data_positions = soup.find_all('td', 'position right nw d0')
+    # global dic
+    rangs_with_names = []
+    html = requests.get(urls).content
+    soup = BeautifulSoup(html, "lxml")
+    f = open('soup.html', 'wb')
+    f.write(soup.prettify().encode('utf8'))
+    data = soup.find_all('div', 'mg_name')
+    positions = soup.find_all('td', 'position right nw d0')
+    rang = [name.string for name in positions]
+    list_names = [name.string for name in data]
+    # print(rang)
+    # print(list_names)
+    for index in range(min(len(rang),len(list_names))):
+        rangs_with_names.append('{}. {}'.format(rang[index], list_names[index]))
+    text = ("\n".join(rangs_with_names))
+    return text
 
 
-	for position in data_positions:
-		print(position)
-	
-	list_names = [name.string for name in data]
-
-	print(list_names)
-
-    
-    
-    
-urls = 'https://www.kicktipp.at/wm-partypatrioten/gesamtuebersicht?ansicht=platzierungen&'
-url = 'https://www.kicktipp.at/wm-partypatrioten/gesamtuebersicht/tipper?ansicht=platzierungen&rankingTeilnehmerId='
-user = '22490855'
-get_data()
---
+++ telegrambot.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Simple Bot to reply to Telegram messages.
+This program is dedicated to the public domain under the CC0 license.
+This Bot uses the Updater class to handle the bot.
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+import kicktipp
+
+text = []
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def rangliste(bot, update):
+    #Send rangliste
+    text = kicktipp.get_data()
+    update.message.reply_text(text)
+
+def von(bot, update):
+    #Send rangliste
+    text = "Gemacht mit Liebe von Dennis & Jasper"
+    update.message.reply_text(text)
+    
+def schwul(bot, update):
+    #Send rangliste
+    text = "#nohomo"
+    update.message.reply_text(text)
+          
+     
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """Start the bot."""
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater("618070298:AAFiESoaLWVTvM151CGFGBgvaxnOPB1yOzo")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("rangliste", rangliste))
+    dp.add_handler(CommandHandler("von", von))
+    dp.add_handler(CommandHandler("schwul", schwul))
+    
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
