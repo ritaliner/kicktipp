5680a16d24ba99a5fd4d83902e9799935e2ac5e3
ritaliner <40183871+ritaliner@users.noreply.github.com>
2018-06-12 12:34:43 +0200

    Update telegrambot.py

--- telegrambot.py
+++ telegrambot.py
@@ -62,7 +62,7 @@ def main():
     dp.add_handler(CommandHandler("rangliste", rangliste))
     dp.add_handler(CommandHandler("von", von))
     dp.add_handler(CommandHandler("schwul", schwul))
-    
+    dp.add_handler(CommandHandler("bezahlt", bezahlt))
     # log all errors
     dp.add_error_handler(error)
 
