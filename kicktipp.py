b5532aca40e65883e178345779c76b7600bc9948
ritaliner <40183871+ritaliner@users.noreply.github.com>
2018-06-14 18:10:35 +0200

    Update kicktipp.py

--- kicktipp.py
+++ kicktipp.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 urls = 'https://www.kicktipp.at/wm-partypatrioten/gesamtuebersicht?ansicht=platzierungen&'
 list_names = 0
 # dic = {}
-
+akturl = 'https://www.kicktipp.de/wm-partypatrioten/tippuebersicht'
 
 def get_data():
     # global dic
@@ -27,4 +27,19 @@ def get_data():
     text = ("\n".join(rangs_with_names))
     return text
 
+def get_akt():
+    rangs_with_names = []
+    html = requests.get(akturl).content
+    soup = BeautifulSoup(html, "lxml")
+    f = open('soup.html', 'wb')
+    f.write(soup.prettify().encode('utf8'))
+    data = soup.find_all('div', 'mg_name')
+    positions = soup.find_all('td', 'position right nw d0')
+    rang = [name.string for name in positions]
+    list_names = [name.string for name in data]
+    for index in range(min(len(rang),len(list_names))):
+        rangs_with_names.append('{}. {}'.format(rang[index], list_names[index]))
+    text = ("\n".join(rangs_with_names))
+    return text
 
+    
